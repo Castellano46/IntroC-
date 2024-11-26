d454a0f16c531d6a483b55fc82ce2397f76c0853
Castellano46 <pedroliebana35@gmail.com>
2024-11-26 10:20:17 +0100

    FirstSteps

--- IntroCsharpBackend/Program.cs
+++ IntroCsharpBackend/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var sale = new SaleWithTax(15);
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+class SaleWithTax : Sale
+{
+    public decimal Tax { get; set; }
+
+    public SaleWithTax(decimal total) : base(total)
+    {
+        Tax = total * 0.15m; // 15% de impuesto
+    }
+
+    public override string GetInfo()
+    {
+        return "El total es " + Total + ", Impuesto es: " + Tax;
+    }
+}
+
+class Sale
+{
+    public decimal Total { get; set; }
+
+    public Sale(decimal total)
+    {
+        Total = total;
+    }
+
+    public virtual string GetInfo()
+    {
+        return "El total es " + Total;
+    }
+}
